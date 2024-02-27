b8df6849c37269e62172e4414462f1c92c2bbf12
vf98 <vf98@AR2-asistent.ftn.ns>
2024-02-27 09:52:04 +0100

    Prve vezbe - 27.02.2024.

---
+++ RvaG4/src/main/java/rva/models/Artikl.java
@@ -0,0 +1,68 @@
+package rva.models;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
+public class Artikl implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@SequenceGenerator(name = "ARTIKL_SEQ_GENERATOR", sequenceName = "ARTIKL_SEQ", 
+	allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ARTIKL_SEQ_GENERATOR" )
+	private int id;
+	
+	
+	private String naziv;
+	private String proizvodjac;
+	
+	@OneToMany(mappedBy = "artikl")
+	private List<StavkaPorudzbine> stavke;
+
+	public Artikl() {
+
+	}
+
+	public Artikl(int id, String naziv, String proizvodjac) {
+		this.id = id;
+		this.naziv = naziv;
+		this.proizvodjac = proizvodjac;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public String getProizvodjac() {
+		return proizvodjac;
+	}
+
+	public void setProizvodjac(String proizvodjac) {
+		this.proizvodjac = proizvodjac;
+	}
+
+}
---
+++ RvaG4/src/main/java/rva/models/Porudzbina.java
@@ -0,0 +1,93 @@
+package rva.models;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
+public class Porudzbina implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@SequenceGenerator(name = "PORUDZBINA_SEQ_GENERATOR", sequenceName = "PORUDZBINA_SEQ", 
+	allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PORUDZBINA_SEQ_GENERATOR" )
+	private int id;
+	private Date datum;
+	private Date isporuceno;
+	private double iznos;
+	private boolean placeno;
+	
+	@OneToMany(mappedBy = "porudzbina")
+	private List<StavkaPorudzbine> stavke;
+	
+	@ManyToOne
+	@JoinColumn(name = "dobavljac")
+	private Dobavljac dobavljac;
+
+	public Porudzbina() {
+
+	}
+
+	public Porudzbina(int id, Date datum, Date isporuceno, double iznos, boolean placeno) {
+		this.id = id;
+		this.datum = datum;
+		this.isporuceno = isporuceno;
+		this.iznos = iznos;
+		this.placeno = placeno;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDatum() {
+		return datum;
+	}
+
+	public void setDatum(Date datum) {
+		this.datum = datum;
+	}
+
+	public Date getIsporuceno() {
+		return isporuceno;
+	}
+
+	public void setIsporuceno(Date isporuceno) {
+		this.isporuceno = isporuceno;
+	}
+
+	public double getIznos() {
+		return iznos;
+	}
+
+	public void setIznos(double iznos) {
+		this.iznos = iznos;
+	}
+
+	public boolean isPlaceno() {
+		return placeno;
+	}
+
+	public void setPlaceno(boolean placeno) {
+		this.placeno = placeno;
+	}
+
+}
